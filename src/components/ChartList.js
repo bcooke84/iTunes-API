b9599c827248241e63087e448d060bfb914ad11b
Brian Cooke <brian.cooke@live.co.uk>
2018-06-27 09:33:27 +0100

    Changed ChartList to stateless component

--- src/components/ChartList.js
+++ src/components/ChartList.js
@@ -2,20 +2,15 @@ import React from 'react';
 import ChartItem from "./ChartItem.js";
 import "../index.css";
 
-class ChartList extends React.Component {
+const ChartList = (props) => {
 
+  const chartListItems = props.chart.map((song, index) => {
+    return <ChartItem key={index} value={index} song={song} songRef={song["link"][1]["attributes"]["href"]}/>
+  })
 
-  // GET ACCESS TO THE PROPS (ARRAY OF SONGS), AND MAP OVER THEM
-  render () {
-    const chartListItems = this.props.chart.map((song, index) => {
-      return <ChartItem key={index} value={index} song={song} songRef={song["link"][1]["attributes"]["href"]}/>
-    })
-
-    return (
-      <div className='chart-list'>{chartListItems}</div>
-    )
-  }
-
+  return (
+    <div className='chart-list'>{chartListItems}</div>
+  )
 }
 
 export default ChartList;
